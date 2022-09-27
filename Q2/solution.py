39d7a3f547f37b6c6d15f572e02a6bf2b0e63cc4
Usman Ahmad <uah5512@gmail.com>
2022-09-28 02:08:22 +0300

    Typo replaced

--- Q2/solution.py
+++ Q2/solution.py
@@ -14,18 +14,18 @@ class Solution:
         trailing_tmp = head
         for curr_idx in range(n):
              
-            # linked-list is exhauseted i.e., len(linked_list) < rm_index
+            # linked-list is exhauseted i.e., len(linked_list) < n
             if not leading_tmp.next:
-                # len(linked_list) == rm_index
+                # len(linked_list) == n
                 if curr_idx == n - 1:
                     head = head.next
                 return head
             leading_tmp = leading_tmp.next
          
-        # exhaust leading pointer until trailing pointer catches up to len(linked-list) - rm_index
+        # exhaust leading pointer until trailing pointer catches up to len(linked-list) - n
         while leading_tmp.next:
             leading_tmp = leading_tmp.next
             trailing_tmp = trailing_tmp.next
         
-        # trailing-node = len(linked_list) - rm_index - 1
+        # trailing-node = len(linked_list) - n - 1
         trailing_tmp.next = trailing_tmp.next.next
