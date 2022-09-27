677ab23a6ff2042122f42557a7a3841faa514db0
Usman Ahmad <uah5512@gmail.com>
2022-09-28 02:16:51 +0300

    code Format

--- Q1/solution.py
+++ Q1/solution.py
@@ -10,10 +10,10 @@ def sum_fact_digit(fact: int) -> int:
     """sum digits in factorial"""
     sum_ = 0
     while fact !=0:
-        sum_ += fact%10
-        fact = int(fact/10)
+        sum_ += fact % 10  # breaking number digit by digit 
+        fact = int(fact / 10)
     return sum_
 
 
 if __name__ == "__main__":
-    print(sum_fact_digit(factorial(99))) # Sum of digits in 99! is same as 100! (only saves some computations)
+    print(sum_fact_digit(factorial(99))) # Sum of digits in 99! is same as 100! (only saves few computations)
